ce7a5a46eec59a3f6bd2ec2baa35c66e9d978edc
Cesar Gonzales <cgonzaless1477@gmail.com>
2025-12-08 19:11:29 -0500

    prueba

--- src/test/java/com/curso/selenium/PrimerTest.java
+++ src/test/java/com/curso/selenium/PrimerTest.java
@@ -24,7 +24,7 @@ public class PrimerTest {
 
             System.out.println("Título de la página: " + driver.getTitle());
 
-            // Aquí tomamos la captura de evidencia aaaabbb
+            // Aquí tomamos la captura de evidencia aaaabbbm
             takeScreenshot(driver, "google-home");
         } finally {
             driver.quit();
